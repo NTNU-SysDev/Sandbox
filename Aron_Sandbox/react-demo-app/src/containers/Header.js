da4c193f1478a8c8644ca90775bd4342a0cf78ed
Girts <girts.strazdins@gmail.com>
2019-01-16 17:32:35 +0100

    Implement proper user table refresh with Redux store Take out REST API calls to a separate module

--- Aron_Sandbox/react-demo-app/src/containers/Header.js
+++ Aron_Sandbox/react-demo-app/src/containers/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Modal from 'react-responsive-modal';
 import logo from '../logo.svg';
 import '../css/App.css';
+import {reduxStore, addUser} from '../redux-storage'
+import {restCreateUser} from "../rest-api";
 
 class Header extends React.Component {
 
@@ -10,6 +12,7 @@ class Header extends React.Component {
         this.state = {
             open: false
         };
+        // Need to manually bind component methods so that we can access 'this' within them
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
         this.createNewUser = this.createNewUser.bind(this);
@@ -23,32 +26,29 @@ class Header extends React.Component {
         this.setState({open: false});
     }
 
+    // This method is called, when the user presses "Create" in the User-add modal form
     createNewUser() {
         const name = document.getElementById('name_field').value;
         const email = document.getElementById('email_field').value;
         const phone = document.getElementById('phone_field').value;
         const age = document.getElementById('age_field').value;
-
-        const api_url = process.env.REACT_APP_BACKEND_URL + "/createUser";
-        fetch(api_url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "name": name,
-                "email": email,
-                "phone": phone,
-                "age": age
-            })
-        }).then(function (response) {
+        const user = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "age": age
+        };
+        const self = this; // Need this inside the callback
+        // Send REST request to create a user. On success, notify Redux store about the change
+        restCreateUser(user, function (response) {
             if (response.ok) {
                 alert("User created!");
-                window.location.reload();
+                reduxStore.dispatch(addUser(user));
+                self.closeModal();
             } else {
-                alert("User already exist");
+                alert("Error, user not created");
             }
-        })
+        });
     }
 
     render() {
@@ -58,7 +58,8 @@ class Header extends React.Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1>This is a simple Demo React App</h1>
-                    <button type="button" onClick={this.openModal} className="green waves-effect waves-light btn-large center">
+                    <button type="button" onClick={this.openModal}
+                            className="green waves-effect waves-light btn-large center">
                         <i className="material-icons right">add_circle</i> Add User
                     </button>
                     <Modal open={open} onClose={this.closeModal} center>
@@ -90,13 +91,13 @@ class Header extends React.Component {
                                 <div className="row">
                                     <div className="col s12 m6 l6 center-align">
                                         <button type="button" className="btn waves-effect btn-large waves-light red"
-                                        onClick={this.closeModal}>
+                                                onClick={this.closeModal}>
                                             Close
                                         </button>
                                     </div>
                                     <div className="col s12 m6 l6 center-align">
                                         <button type="button" className="green btn waves-effect btn-large waves-light"
-                                        onClick={this.createNewUser}>
+                                                onClick={this.createNewUser}>
                                             Create
                                             <i className="material-icons right">add_circle</i>
                                         </button>
